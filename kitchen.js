1433f823c16a944dfb748e68d8b11c0bd3973ae2
TiagoJake <139134330+TiagoJake@users.noreply.github.com>
2024-05-15 15:24:26 +0800

    Add files via upload

---
+++ kitchen.js
@@ -0,0 +1,29 @@
+function addsauce1(){
+    document.getElementById("pizzaslice").src = "Pizzawithsacue.png";
+    document.getElementById("pizzaslice").style.width = "1100px";
+}
+
+function addcheese1(){
+    document.getElementById("pizzaslice").src = "pizzawithcheese.png";
+    document.getElementById("pizzaslice").style.width = "1100px";
+}
+
+function addpine1(){
+    document.getElementById("pizzaslice").src = "pizzawithpineapple.png";
+    document.getElementById("pizzaslice").style.width = "1100px";
+}
+
+function addpepporoni(){
+    document.getElementById("pizzaslice").src = "pizzawithpepporoni.png";
+    document.getElementById("pizzaslice").style.marginBottom = "200px";
+    document.getElementById("pizzaslice").style.width = "1500px";
+    document.getElementById("pizzaslice").style.height = "800px";
+
+}
+
+function addtomato(){
+    document.getElementById("pizzaslice").src = "pizzawithtomat.png";
+    document.getElementById("pizzaslice").style.width = "1300px";
+
+
+}
